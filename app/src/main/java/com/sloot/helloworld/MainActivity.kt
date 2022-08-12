203e6d8848d0c9c77af454d67108306eae6542fc
Suyai Slootmans <suyaislootmans@gmail.com>
2022-08-11 21:22:29 -0300

    optimización

--- app/src/main/java/com/sloot/helloworld/MainActivity.kt
+++ app/src/main/java/com/sloot/helloworld/MainActivity.kt
@@ -8,25 +8,26 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        diceImage = findViewById(R.id.dice_image)
 
-       // val countUpButton: Button = findViewById<Button>(R.id.count_up_button)
-       // val restButton: Button = findViewById<Button>(R.id.reset_button)
-       // countUpButton.setOnClickListener { countUp() }
-       // restButton.setOnClickListener { rest() }
+        // val countUpButton: Button = findViewById<Button>(R.id.count_up_button)
+        // val restButton: Button = findViewById<Button>(R.id.reset_button)
+        // countUpButton.setOnClickListener { countUp() }
+        // restButton.setOnClickListener { rest() }
     }
 
     private fun rollDice() {
         // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val randomInt = (1..6).random()
-        val diceImage: ImageView = findViewById<ImageView>(R.id.dice_image)
-
-        val drawableResource = when (randomInt){
+        val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -40,20 +41,20 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-   /* private fun countUp() {
-        val resultText: TextView = findViewById<TextView>(R.id.result_text)
+    /* private fun countUp() {
+         val resultText: TextView = findViewById<TextView>(R.id.result_text)
 
-        if (resultText.text.toString() == "Hello World!") {
-            resultText.text = "1"
-        } else if (resultText.text.toString() == "6") {
-            return
-        } else {
-            resultText.text = (resultText.text.toString().toInt() + 1).toString()
-        }
-    }
+         if (resultText.text.toString() == "Hello World!") {
+             resultText.text = "1"
+         } else if (resultText.text.toString() == "6") {
+             return
+         } else {
+             resultText.text = (resultText.text.toString().toInt() + 1).toString()
+         }
+     }
 
-    private fun rest() {
-        val resultText: TextView = findViewById<TextView>(R.id.result_text)
-        resultText.text = "0"
-    } */
+     private fun rest() {
+         val resultText: TextView = findViewById<TextView>(R.id.result_text)
+         resultText.text = "0"
+     } */
 }
